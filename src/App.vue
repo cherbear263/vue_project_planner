11cc2de90688a669ba75a4095865d6920cc00738
msjones3 <cheryl@realsocial.com.au>
2022-03-20 21:54:05 +1000

    toggle project details

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="md:max-w-md md:mx-auto max-w-full mx-4 ">
-<p>Here is some text</p>
+
   <router-view/>
   </div>
 </template>
---
+++ src/components/SingleProject.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-left container my-3 mx-auto bg-white py-2 px-4 rounded-md shadow border-l-8 border-pink-600/50">
+    <div class="actions">
+      <h3 class="text-lg font-semibold cursor-pointer"
+      @click="showDetails = !showDetails">{{ project.title }}</h3>
+
+    </div>
+  <div v-if="showDetails">
+    <p>{{ project.details }}</p>
+  </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SingleProject',
+  props: ['project'],
+  data() {
+    return {
+      showDetails: false
+    }
+  }
+}
+</script>
